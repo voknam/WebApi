6dd373939932e2938e9e6e6b8dad46e40037e2c6
Voknam <ardy544752@gmail.com>
2020-09-14 21:23:06 +0700

    tambah databse

---
+++ db_kontak.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2020 at 08:44 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kontak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telepon`
+--
+
+CREATE TABLE `telepon` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nomor` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `telepon`
+--
+
+INSERT INTO `telepon` (`id`, `nama`, `nomor`) VALUES
+(1, 'ardy', '081234567898'),
+(2, 'ramadhan', '087363636363');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `telepon`
+--
+ALTER TABLE `telepon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `telepon`
+--
+ALTER TABLE `telepon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
